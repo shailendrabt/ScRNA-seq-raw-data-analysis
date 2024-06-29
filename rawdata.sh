10a1ec357b121589db86fb7edbd05be026e452c1
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-06-29 13:41:41 +0530

    Add files via upload

---
+++ rawdata.sh
@@ -0,0 +1,4 @@
+###### Raw reads download from ENA
+###### open terminal
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR930/008/SRR9304738/SRR9304738.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR930/000/SRR9304740/SRR9304740.fastq.gz
